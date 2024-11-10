a9f79384473d30f0e2c25f4724193b09e69dc05d
Yurii Bech <bechladen@gmail.com>
2024-11-10 15:00:52 -0330

    burger color

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,6 +1,4 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-  // Забезпечення плавної появи всього блоку HERO
   const heroSection = document.querySelector('.HeroSection');
   heroSection.style.opacity = '1';
   heroSection.style.transform = 'translateY(0)';
