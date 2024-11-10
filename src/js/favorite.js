9640b73f96b3f652bd9cdd48d7f019d7f9ebbbd7
VladPom <lekar89lekar89@gmail.com>
2024-11-10 14:44:33 +0200

    work on task

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,173 +1,200 @@
-// import icon from '../img/sprite.svg';
-// import axios from "axios";
-// import { addFavoriteCards, deleteCard, getAllFavoriteCards } from "./local-storage";
-// import { makePaginationByItems } from './pagination.js';
-// import { handleOpenModalClick } from './modal-exercise';
-// import { processRemovalsFromFavorites} from './modal-exercise';
+import icons from '../img/icons/sprites.svg';
+import axios from "axios";
+import { addFavoriteCards, deleteCard, getAllFavoriteCards } from "./local-storage";
+import { makePaginationByItems } from './pagination.js';
+import { api } from './api';
 
 
-// async function renderQuote() {
-//     const currentDate = new Date();
-//     const currentDay = currentDate.getDate();
 
-//     let storedQuoteState = JSON.parse(localStorage.getItem("quoteData")) ?? {};
+async function renderQuote() {
+    const currentDate = new Date();
+    const currentDay = currentDate.getDate();
 
-//     const storedQuoteDay = storedQuoteState.day ?? 0;
+    let storedQuoteState = JSON.parse(localStorage.getItem("quoteData")) ?? {};
 
-//     if (currentDay !== storedQuoteDay) {
-//         const quoteData = await serviceQuote();
+    const storedQuoteDay = storedQuoteState.day ?? 0;
 
-//         quoteData.day = currentDay;
-//         localStorage.setItem("quoteData", JSON.stringify(quoteData));
-//         storedQuoteState = quoteData;
-//     }
+    if (currentDay !== storedQuoteDay) {
+        const quoteData = await serviceQuote();
 
-//     const quoteText = document.querySelector(".quote-text"); 
-//     const quoteAuthor = document.querySelector(".quote-author");
-//     quoteText.textContent = storedQuoteState.quote; 
-//     quoteAuthor.textContent = storedQuoteState.author;
+        quoteData.day = currentDay;
+        localStorage.setItem("quoteData", JSON.stringify(quoteData));
+        storedQuoteState = quoteData;
+    }
 
-// }
+    const quoteText = document.querySelector(".quote-text"); 
+    const quoteAuthor = document.querySelector(".quote-author");
+    quoteText.textContent = storedQuoteState.quote; 
+    quoteAuthor.textContent = storedQuoteState.author;
 
-// async function serviceQuote() {
-//   try {
-//     const response = await axios.get(
-//       `https://your-energy.b.goit.study/api/quote`
-//     );
-//     return response.data; 
-//   } catch (error) {
-//     return null;
-//   }
-// }
+}
 
-// renderQuote();
+async function serviceQuote() {
+  try {
+    const response = await axios.get(
+      `https://your-energy.b.goit.study/api/quote`
+    );
+    return response.data; 
+  } catch (error) {
+    return null;
+  }
+}
+
+renderQuote();
 
 
-// const Refs = {
-//   galleryList: document.querySelector(".gallery-list"),
-//   defaultText: document.querySelector(".js-hidden-text"),
-// }
+const Refs = {
+  galleryList: document.querySelector(".gallery-list"),
+  defaultText: document.querySelector(".js-hidden-text"),
+}
 
-// function createExerciseCards(exercises) {
+function createExerciseCards(exercises) {
   
-//     return exercises.map(({_id, bodyPart, burnedCalories, target, name, time}) => 
-//       `<li data-id="${_id}" class="exercise-card ">
-//         <div class="exersise-header">
-//           <div class="exercise-trash">
-//             <p class="workout">WORKOUT</p>
-//           <button class="trash-btn" type="submit">
-//           <svg class="trash-svg"  width="16" height="16">
-//                 <use id = "dell" href="${icon}#icon-trash-modal"></use>
-//                 </svg> 
-//           </button>              
-//           </div>    
-//             <button id = "open" class="exercise-btn" type="button">Start
-//               <svg id = "arrow" class="arrow-svg" width="16" height="16">
-//                 <use href="${icon}#icon-arrow"></use>
-//               </svg>
-//             </button>
-//         </div>  
-//           <div class = "exercise-tittle"> 
-//           <div class= "man-svg-thumb">
-//             <svg width="24" height="24">
-//               <use href="${icon}#icon-icon-2"></use>
-//             </svg>
-//             </div>
-//             <p class="favorite-exercise-name">${capitalize(name)}</p>
-//           </div> 
-//           <div class="exercice-information">
-//             <p class="exercise-category">Burned calories: <span>${burnedCalories}/${time}min</span></p>
-//             <p class="exercise-category">Body part: <span>${bodyPart}</span></p>
-//             <p class="exercise-category">Target: <span>${target}</span></p>
-//           </div>            
-//       </li>`
-//     ).join("");
-// }
+    return exercises.map(({_id, bodyPart, burnedCalories, target, name, time}) => 
+      `<li data-id="${_id}" class="exercise-card">
+        <div class="exercise-header">
+          <div class="exercise-trash">
+            <p class="workout">WORKOUT</p>
+          <button class="trash-btn" type="submit">
+          <svg class="trash-svg"  width="16" height="16">
+                <use id = "dell" href="${icons}#trash-bin"></use>
+                </svg> 
+          </button>              
+          </div>    
+            <button id = "open" class="exercise-btn" type="button">Start
+              <svg id = "arrow" class="arrow-svg" width="16" height="16">
+                <use href="${icons}#exercise-start"></use>
+              </svg>
+            </button>
+        </div>  
+          <div class = "exercise-tittle"> 
+          <div class= "man-svg-thumb">
+            <svg width="24" height="24">
+              <use href="${icons}#icon-icon-2"></use>
+            </svg>
+            </div>
+            <p class="favorite-exercise-name">${capitalize(name)}</p>
+          </div> 
+          <div class="exercise-information">
+            <p class="exercise-category">Burned calories: <span>${burnedCalories}/${time}min</span></p>
+            <p class="exercise-category">Body part: <span>${bodyPart}</span></p>
+            <p class="exercise-category">Target: <span>${target}</span></p>
+          </div>            
+      </li>`
+    ).join("");
+}
 
-// function renderExercises(exercises) {
-//   let cards = createExerciseCards(exercises);
 
-//   Refs.galleryList.innerHTML=""
-//   Refs.galleryList.insertAdjacentHTML('beforeend', cards);
+async function  getAllFavoriteModels(exercises){
+  let res = exercises.map(async(x) => await api.exerciseInfo(x))
+  let exModels  = []
+  const exerciseData =  api.exerciseInfo(exerciseId);
+  res.forEach(async(element) => element.then((value) => {
+    exModels.add(value);
+  }));
+  exModels.forEach((element) => console.log( element));
+}
 
-//   Refs.defaultText.style.display = 'none';
-//   Refs.galleryList.style.display = 'flex';
+async function  renderExercises(exercises)  {
+let exModels = getAllFavoriteModels(exercises)
+  exModels.forEach((element) => console.log( element));
+  // exercises.forEach(async(element) => console.log(await api.exerciseInfo(element)));
+
+//   api.exerciseInfo(x)
+//   .then((value) => {
+//    console.log(value);
+//    // Expected output: "Success!"
+//  });
+  
+
+// 
+ 
+  let cards = createExerciseCards(exercises);
+
+  Refs.galleryList.innerHTML=""
+  Refs.galleryList.insertAdjacentHTML('beforeend', cards);
+
+  Refs.defaultText.style.display = 'none';
+  Refs.galleryList.style.display = 'flex';
     
-//   const cardsEl = document.querySelectorAll(".exercise-card"); 
-//   for (let card of cardsEl) {
-//     card.addEventListener('click', handleCardClick);  
-//   }
+  const cardsEl = document.querySelectorAll(".exercise-card"); 
+  for (let card of cardsEl) {
+    card.addEventListener('click', handleCardClick);  
+  }
 
-//   if (exercises.length === 0) {
-//     Refs.defaultText.style.display = 'flex';
-//     Refs.galleryList.style.display = 'none';    
-//  } 
-// }
+  if (exercises.length === 0) {
+    Refs.defaultText.style.display = 'flex';
+    Refs.galleryList.style.display = 'none';    
+ } 
+}
 
-// let perPage = 7
-// window.addEventListener("resize", renderCards)
-// function renderCards() {
-//   let exercises = getAllFavoriteCards();
-  
 
-//   if (window.innerWidth >= 768) {
+
+let perPage = 7
+window.addEventListener("resize", renderCards)
+ function renderCards() {
+  let exercises = getAllFavoriteCards();
+  console.log(exercises)
+  console.log(exercises)
+
+  if (window.innerWidth >= 768) {
     
-//     perPage = 10; 
+    perPage = 10; 
   
-//   }
+  }
 
-//   if (window.innerWidth >= 1440) {
+  if (window.innerWidth >= 1440) {
    
-//     perPage = 6; 
+    perPage = 6; 
   
-//   }
+  }
 
-//   renderExercises(exercises.slice(0, perPage))
+  renderExercises(exercises.slice(0, perPage))
 
-//   makePaginationByItems(perPage, exercises.length).on(
-//     'afterMove',
-//     ({ page }) => {
-//         let start = (page-1)*perPage
-//       let end = page * perPage;
+  makePaginationByItems(perPage, exercises.length).on(
+    'afterMove',
+    ({ page }) => {
+        let start = (page-1)*perPage
+      let end = page * perPage;
       
-//   renderExercises(exercises.slice(start, end))
-//     });
-// }
+  renderExercises(exercises.slice(start, end))
+    });
+}
 
-// renderCards(); 
+renderCards(); 
 
-// const cards = document.querySelectorAll(".exercise-card"); 
+const cards = document.querySelectorAll(".exercise-card"); 
 
-// for (let card of cards) {
-//   card.addEventListener('click', handleCardClick);
-// }
+for (let card of cards) {
+  card.addEventListener('click', handleCardClick);
+}
 
-// async function handleCardClick(event) {
-//   switch (event.target.id) {
-//     case "dell":
-//       return removeCard(event);
-//     case "open":
-//       return openCard(event);
-//     case "arrow":
-//       return openCard(event);
-//   }
-// }
+async function handleCardClick(event) {
+  switch (event.target.id) {
+    case "dell":
+      return removeCard(event);
+    case "open":
+      return openCard(event);
+    case "arrow":
+      return openCard(event);
+  }
+}
 
-// function removeCard(event) {
-//   const id = event.currentTarget.dataset.id; 
-//   deleteCard(id);
-//   processRemovalsFromFavorites(id, event);
-//   renderCards();  
-// }
+function removeCard(event) {
+  const id = event.currentTarget.dataset.id; 
+  deleteCard(id);
+  removeFromFavorites(id, event);
+  renderCards();  
+}
 
-// async function openCard(event) {
-//   const id = event.currentTarget.dataset.id; 
+async function openCard(event) {
+  const id = event.currentTarget.dataset.id; 
     
-//   handleOpenModalClick(event, id);
-// }
+  handleOpenModalClick(event, id);
+}
 
-// function capitalize(s) {
-//   return s[0].toUpperCase() + s.slice(1);
-// }
+function capitalize(s) {
+  return s;
+}
 
 
---
+++ src/js/pagination.js
@@ -0,0 +1,69 @@
+import Pagination  from "tui-pagination";
+import 'tui-pagination/dist/tui-pagination.css';
+
+function makePagination(perPage, totalPages) {
+
+  const paginationEl = document.getElementById('tui-pagination-container');  
+  const visiblePages = totalPages < 5 ? totalPages : 5;
+  const options = {
+    totalItems:perPage*totalPages,
+    itemsPerPage: perPage,
+    visiblePages,
+    centerAlign: true,
+  };
+ 
+  const pagination = new Pagination(paginationEl, options);
+
+  if (visiblePages <= 1) {
+    paginationEl.style.display = 'none';
+  } else {
+    paginationEl.style.display = 'block';
+  }
+
+  return pagination;
+}
+
+function makePaginationByItems (perPage, totalItems) {
+  const totalPages = totalItems / perPage + 1; 
+
+  
+  const paginationEl = document.getElementById('tui-pagination-container');  
+  const visiblePages = totalPages < 5 ? totalPages : 5;
+  const options = {
+    totalItems,
+    itemsPerPage: perPage,
+    visiblePages,
+    centerAlign: true,
+  };
+ 
+  const pagination = new Pagination(paginationEl, options);
+
+  if (visiblePages <= 1) {
+    paginationEl.style.display = 'none';
+  } else {
+    paginationEl.style.display = 'block';
+  }
+
+  return pagination;
+}
+
+
+
+
+
+export { makePagination, makePaginationByItems };
+    
+//  <div id="tui-pagination-container" class="tui-pagination"></div> 
+    
+// makePagination(12, data.totalPages).on(
+//     'afterMove',
+//     ({ page }) => {
+//        renderCards(filter, page)
+//     });
+
+//     async function renderCards(filter, page) {
+//     const data = await fetchData(filter, page);
+//     if (data) {
+//       gallery.insertAdjacentHTML("beforeend", createMarcup(data.results));
+//     }
+// }
