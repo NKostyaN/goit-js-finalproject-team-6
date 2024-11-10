a801f1c6c1fc154f49de20e3d42a32782e996145
Kos <kosnesterenko@gmail.com>
2024-11-11 00:55:53 +0200

    hero fixed

--- src/js/hero.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Забезпечення плавної появи всього блоку HERO
-  const heroSection = document.querySelector('.HeroSection');
-  heroSection.style.opacity = '1';
-  heroSection.style.transform = 'translateY(0)';
-});
