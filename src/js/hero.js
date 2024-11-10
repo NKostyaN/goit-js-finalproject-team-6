b199454c3a0a6f762eb982cf72e47659c8d3f2d3
Kos <kosnesterenko@gmail.com>
2024-11-10 17:59:29 +0200

    menu fix

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,8 +1,7 @@
-// src/js/hero.js
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   // Забезпечення плавної появи всього блоку HERO
-  const heroSection = document.querySelector(".HeroSection");
-  heroSection.style.opacity = "1";
-  heroSection.style.transform = "translateY(0)";
+  const heroSection = document.querySelector('.HeroSection');
+  heroSection.style.opacity = '1';
+  heroSection.style.transform = 'translateY(0)';
 });
