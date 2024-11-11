c3f966b1d53f48bbe8d346a0acfea4192520c0cf
NKostyaN <kosnesterenko@gmail.com>
2024-11-11 14:01:39 +0000

    Deploying to gh-pages from @ NKostyaN/goit-js-finalproject-team-6@fbe691552351a76cd827ec536d2c2fa16d436567 🚀

--- favorites.js
+++ favorites.js
@@ -1,29 +1,31 @@
-import{g as h,t as x,b as u,c as d,a as p}from"./assets/utils-CpFMTryu.js";import"./assets/vendor-CNNbG8jS.js";async function m(){function e(){return fetch("https://your-energy.b.goit.study/api/quote").then(o=>{if(!o.ok)throw new Error(`Fetch error with ${o.status}: ${o.statusText}`);return o.json()})}const s=new Date().getDate();let r=JSON.parse(localStorage.getItem("quoteData"))??{};const c=r.day??0;if(s!==c){const a=await e();a.day=s,localStorage.setItem("quoteData",JSON.stringify(a)),r=a}const i=document.querySelector(".quote-text"),l=document.querySelector(".quote-author");i.textContent=r.quote,l.textContent=r.author}m();const n={galleryList:document.querySelector(".gallery-list"),defaultText:document.querySelector(".js-hidden-text")};async function v(e){return await Promise.all(e.map(async s=>await p.exerciseInfo(s)))}async function w(e){return v(e).then(t=>t.map(({_id:s,bodyPart:r,burnedCalories:c,target:i,name:l,time:a})=>`<li data-id="${s}" class="exercise-card">
-            <div class="exercise-header">
-              <div class="exercise-trash">
-                <p class="exercise-workout">WORKOUT</p>
-                <button class="trash-btn" type="submit">
-                  <svg class="trash-svg"  width="16" height="16">
-                    <use id = "dell" href="${u}#icon-trash-fav"></use>
-                  </svg> 
-                </button> 
-              </div>
-              <button id = "open" class="exercise-btn" type="button">
-                Start
-                <svg class="exercise-arrow" width="13" height="13">
-                  <use href="${u}#icon-start-arrow"></use>
-                </svg>
-              </button>
-            </div>
-            <h3 class="exercise-name">
-              <svg class="exercise-name-icon" width="24" height="24">
-                <use href="${u}#icon-icon-2"></use>
-              </svg>${d(l)}</h3>
-            <div class="exercise-info">
-              <p class="truncate-text"><strong class="exercise-info-title">Burned calories:</strong> ${c}/${a}min</p>
-              <p class="truncate-text"><strong class="exercise-info-title">Body part:</strong> ${d(r)}</p>
-              <p class="truncate-text"><strong class="exercise-info-title">Target:</strong> ${d(i)}</p>
-            </div>
-          </li>
-          `).join(""))}function q(e){w(e).then(t=>{n.galleryList.innerHTML="",n.galleryList.insertAdjacentHTML("beforeend",t),n.defaultText.style.display="none",n.galleryList.style.display="flex";const s=document.querySelectorAll(".exercise-card");for(let r of s)r.addEventListener("click",f);e.length===0&&(n.defaultText.style.display="flex",n.galleryList.style.display="none")},t=>{console.log(t)})}window.addEventListener("resize",g);function g(){let e=h();q(e)}g();const S=document.querySelectorAll(".exercise-card");for(let e of S)e.addEventListener("click",f);async function f(e){switch(e.target.id){case"dell":return T(e);case"open":return y(e);case"arrow":return y(e)}}function T(e){const t=e.currentTarget.dataset.id;x(t),g()}async function y(e){e.currentTarget.dataset.id}
+import{g,i as h,t as p,b as u,c as d,a as v}from"./assets/utils-BZdYtfRu.js";import"./assets/vendor-CNNbG8jS.js";async function f(){function e(){return fetch("https://your-energy.b.goit.study/api/quote").then(n=>{if(!n.ok)throw new Error(`Fetch error with ${n.status}: ${n.statusText}`);return n.json()})}const s=new Date().getDate();let a=JSON.parse(localStorage.getItem("quoteData"))??{};const i=a.day??0;if(s!==i){const r=await e();r.day=s,localStorage.setItem("quoteData",JSON.stringify(r)),a=r}const c=document.querySelector(".quote-text"),l=document.querySelector(".quote-author");c.textContent=a.quote,l.textContent=a.author}f();const o={galleryList:document.querySelector(".gallery-list"),defaultText:document.querySelector(".js-hidden-text")};async function x(e){return await Promise.all(e.map(async s=>await v.exerciseInfo(s)))}async function m(e){return x(e).then(t=>t.map(({_id:s,bodyPart:a,burnedCalories:i,target:c,name:l,time:r})=>`<li data-id="${s}" class="exercise-card">
+                  <div class="exercise-header">
+                    <div class="exercise-trash">
+                      <p class="workout">WORKOUT</p>
+                    <button id="dell" class="trash-btn" type="submit">
+                    <svg class="trash-svg"  width="16" height="16">
+                          <use  href="${u}#icon-trash-fav"></use>
+                          </svg> 
+                    </button>              
+                    </div>    
+                      <button class="exercise-btn" type="button" data-modal-open value="${s}">Start
+                        <svg  class="arrow-svg" width="16" height="16">
+                          <use href="${u}#icon-arrow"></use>
+                        </svg>
+                      </button>
+                  </div>  
+                    <div class = "exercise-tittle"> 
+                    <div class= "man-svg-thumb">
+                      <svg width="24" height="24">
+                        <use href="${u}#icon-icon-2"></use>
+                      </svg>
+                      </div>
+                      <p class="favorite-exercise-name">${d(l)}</p>
+                    </div> 
+                    <div class="exercise-information">
+                      <p class="exercise-category">Burned calories: <span>${i}/${r}min</span></p>
+                      <p class="exercise-category">Body part: <span>${d(a)}</span></p>
+                      <p class="exercise-category">Target: <span>${d(c)}</span></p>
+                    </div>            
+                </li>`).join(""))}function q(e){m(e).then(t=>{o.galleryList.innerHTML="",o.galleryList.insertAdjacentHTML("beforeend",t),o.defaultText.style.display="none",o.galleryList.style.display="flex";const s=document.querySelectorAll(".exercise-card");for(let a of s)a.querySelector("#dell").addEventListener("click",w);e.length===0&&(o.defaultText.style.display="flex",o.galleryList.style.display="none"),h()},t=>{console.log(t)})}window.addEventListener("resize",y);async function y(){let e=g();await q(e)}y();function w(e){const t=document.querySelector(".exercise-card").dataset.id;p(t),y()}
 //# sourceMappingURL=favorites.js.map
