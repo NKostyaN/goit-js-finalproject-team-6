c73e70aac842fe3cd0e69d78907a66690cb4f743
boogie1989 <serhiibuhai.work@gmail.com>
2024-11-11 18:44:49 +0200

    try to fix

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   const navLinks = document.querySelectorAll('.nav-link');
 
-  let currentPath = window.location.pathname;
-  if (currentPath == '/') {
-    currentPath = '/index.html';
+  const parts = window.location.pathname.split('/');
+  let currentPath;
+  if (parts.length == 0) {
+    currentPath = 'index.html';
+  } else {
+    currentPath = parts[parts.length - 1];
   }
+
   navLinks.forEach(link => {
     if (link.getAttribute('href').endsWith(currentPath)) {
       link.classList.add('active');
