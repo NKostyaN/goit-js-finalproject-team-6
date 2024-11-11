243821631bd0f50339b5e254bb6af7e40d069eb5
boogie1989 <serhiibuhai.work@gmail.com>
2024-11-11 18:35:35 +0200

    try to fix navigation

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,9 +1,17 @@
-const currentPath = window.location.pathname;
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-link');
 
-const navLinks = document.querySelectorAll('.nav-link');
+  let currentPath = window.location.pathname;
+  if (currentPath == '/') {
+    currentPath = '/index.html';
+  }
+  navLinks.forEach(link => {
+    if (link.getAttribute('href').endsWith(currentPath)) {
+      link.classList.add('active');
+    }
+  });
 
-navLinks.forEach(link => {
-  if (link.getAttribute('href') === `.${currentPath}`) {
-    link.classList.add('active');
+  if (!document.querySelector('.nav-link.active')) {
+    navLinks[0].classList.add('active');
   }
 });
